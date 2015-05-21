38eda53d21dd9d0c8416c95c6113601a1d01e7a3
WangErXiao <863579016@qq.com>
2015-05-21 23:06:19 +0800

    chapter06

---
+++ chapter06/bigpy-tree.py
@@ -0,0 +1,27 @@
+import sys,os,pprint
+trace=False
+print('system platform: %s ' % sys.platform)
+if sys.platform.startswith('win'):
+    dirname=''
+else:
+    dirname='/usr/lib/python2.7'
+
+allsizes=[]
+for (thisDir,subsHere,filesHere) in os.walk(dirname):
+    if trace:print(thisDir)
+    for filename in filesHere:
+        if filename.endswith('.py'):
+            if trace:print('...',filename)
+            fullname=os.path.join(thisDir,filename)
+            fullsize=os.path.getsize(fullname)
+            allsizes.append((fullsize,fullname))
+
+allsizes.sort()
+pprint.pprint(allsizes[:2])
+pprint.pprint(allsizes[-2:])
+
+
+
+
+
+
