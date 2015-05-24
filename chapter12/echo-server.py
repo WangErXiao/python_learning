5778e8166b9043dd99f71916763b07e907f82082
WangErXiao <863579016@qq.com>
2015-05-24 18:22:39 +0800

    chapter12

---
+++ chapter12/echo-server.py
@@ -0,0 +1,15 @@
+from socket import *
+myHost='localhost'
+myPort=9999
+sockobj=socket(AF_INET,SOCK_STREAM)
+sockobj.bind((myHost,myPort))
+sockobj.listen(5)
+
+while True:
+    connection,address=sockobj.accept()
+    print('Server connected by',address)
+    while True:
+        data=connection.recv(1024)
+        if not data:break
+        connection.send(b'Echo=>'+data)
+    connection.close()
